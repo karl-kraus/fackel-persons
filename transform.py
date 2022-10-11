b62ca9db1caa041580298fd07d8a153bf6ae7f03
csae8092 <peter.andorfer@oeaw.ac.at>
2022-10-11 08:32:24 +0200

    bugfix

--- transform.py
+++ transform.py
@@ -2,13 +2,13 @@ import lxml.etree as ET
 
 from acdh_tei_pyutils.tei import TeiReader
 
-from config import LISTPERSON
+from config import LISTPERSON, FINAL
 
 transform = ET.XSLT(ET.parse('./xslt/tolistperson.xsl'))
 
 doc = TeiReader(LISTPERSON)
 result = transform(doc.tree)
-with open(LISTPERSON, 'w') as f:
+with open(FINAL, 'w') as f:
     content = str(result).replace(
         '<TEI>',
         '<TEI xmlns="http://www.tei-c.org/ns/1.0">'
